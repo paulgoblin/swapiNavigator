dd2ae3c9bf1d8ac793355b4f87af4d271693450c
Michael Richter <michaelrichter@Michaels-MacBook-Pro-2.local>
2015-11-23 21:58:52 -0800

    'add resident name display on planets page.'

--- js/residents.js
+++ js/residents.js
@@ -4,16 +4,17 @@ app.controller("ResidentCtrl", ['$scope', '$stateParams', 'ResidentSrvc', functi
 
   $scope.character = ResidentSrvc.residents[$stateParams.id] ||
     ResidentSrvc.getResident($stateParams.id).then(resp => {
-      $scope.character = ResidentSrvc.addResident(resp.data, $stateParams.id);
+      $scope.character = ResidentSrvc.addResident(resp.data, $stateParams);
     });
 
-}]).service('ResidentSrvc', ['$http', function($http){
+}])
+app.service('ResidentSrvc', ['$http', function($http){
   this.residents = {};
   this.getResident = function(key){
     return $http.get("http://swapi.co/api/people/" + key + "/?format=json")
   }
-  this.addResident = function(data, key) {
-    this.residents[key] = data;
+  this.addResident = function(data, params) {
+    this.residents[params.id] = data;
     return data
   }
 
