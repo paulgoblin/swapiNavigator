fce60ec35e1cfd98ee92ea0aba227b21c33e4ac7
Paul Richter <mpr75@cornell.edu>
2015-11-23 22:16:21 -0800

    'add multiple planet page access'

--- js/planets.js
+++ js/planets.js
@@ -1,25 +1,28 @@
 var app = angular.module('swapiApp');
 
-app.controller("PlanetCtrl", ['$scope', 'PlanetsSrvc', 'ResidentSrvc', function($scope, PlanetsSrvc, ResidentSrvc) {
+app.controller("PlanetCtrl", ['$scope', 'PlanetsSrvc', 'ResidentSrvc', '$stateParams', function($scope, PlanetsSrvc, ResidentSrvc, $stateParams) {
 
-  $scope.planets = PlanetsSrvc.planets;
+  $scope.planets = PlanetsSrvc.planets[$stateParams.page];
   console.log($scope.planets)
   $scope.knownResidents = ResidentSrvc.residents;
 
-  if ($scope.planets.length === 0){
-    PlanetsSrvc.getPlanets.then(response => {
-      $scope.planets = PlanetsSrvc.addPlanets(response);
+  if (!$scope.planets){
+    PlanetsSrvc.getPlanets($stateParams.page).then(response => {
+      $scope.planets = PlanetsSrvc.addPlanets(response, $stateParams.page);
     }).catch(error => console.error(error.status))
   }
 
 }]).service('PlanetsSrvc',[ '$http', function($http){
 
-  this.planets = [];
-  this.getPlanets = $http.get("http://swapi.co/api/planets/?format=json")
+  this.planets = {};
 
-  this.addPlanets = function(response) {
-    console.log('response ', response)
-    this.planets = response.data.results.map(planet => {
+  this.getPlanets = function(page){
+    return $http.get("http://swapi.co/api/planets/?page=" + page + "&format=json")
+  };
+
+  this.addPlanets = function(response, page) {
+    console.log('response ', response.data.results)
+    this.planets[page] = response.data.results.map(planet => {
       planet.residents = planet.residents.map(resident => {
         var resident = { url: resident };
         resident.id = resident.url.match(/\d+/)[0];
@@ -27,7 +30,7 @@ app.controller("PlanetCtrl", ['$scope', 'PlanetsSrvc', 'ResidentSrvc', function(
       });
       return planet;
     });
-    return this.planets
-  }
+    return this.planets[page]
+  };
 
 }])
