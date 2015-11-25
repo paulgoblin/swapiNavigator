ddca39acc71d7d800145de855c1cb1bb0ef67031
Paul Richter <mpr75@cornell.edu>
2015-11-24 17:10:29 -0800

    'add minifyer gulp.js'

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var del = require('del');
+var uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+
+gulp.task('build', ['clean', 'js']);
+
+gulp.task('clean', function(){
+  del('dist')
+})
+
+gulp.task('js', function(){
+  gulp.src('js/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+})
+
+
+gulp.task('default',['build']);
